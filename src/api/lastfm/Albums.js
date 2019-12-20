55ed80d841604f657cb11526cd390b87b5ca8f45
Ryan Krol <ryankr@hotmail.co.uk>
2019-12-20 01:58:26 +0000

    Implement extractRelevantApiData to pull data in the right format

--- src/api/lastfm/Albums.js
+++ src/api/lastfm/Albums.js
@@ -8,7 +8,27 @@ const API_ENDPOINT = `http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums
 
 async function fetchAlbums() {
   const rawAlbumData = await apiCall()
-  return rawAlbumData
+  const resultData = extractRelevantApiData(rawAlbumData)
+
+  return resultData
+}
+
+function extractRelevantApiData(rawData) {
+  const albums = rawData.topalbums.album
+  const relevantdata = albums.map((album) => {
+    const artist = album.artist.name
+    const albumName = album.name
+    const thumbnail = album.image[album.image.length-1]['#text']
+
+    if (artist && albumName && thumbnail)
+      return {
+        artist,
+        albumName,
+        thumbnail,
+      }
+  }).filter((x) => typeof x !== 'undefined')
+
+  return relevantdata
 }
 
 async function apiCall() {
