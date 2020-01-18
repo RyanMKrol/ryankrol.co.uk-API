29e5379adb6c4f8ada2672d187bb112e34b876d9
Ryan Krol <ryankr@hotmail.co.uk>
2020-01-18 20:10:31 +0000

    Implement multiple API calls to fetch all books
    * The GoogleBooks API only returns a max of 40 items
    per call, so I'm now making one call to get the information
    of the "bookshelf", which will tell me how many books there are.
    Using that information I can then send out multiple calls
    to fetch the information on all of the books.

--- src/api/GoogleBooks/Books.js
+++ src/api/GoogleBooks/Books.js
@@ -1,22 +1,49 @@
+import ceil from 'lodash.ceil'
+import fill from 'lodash.fill'
 import fs from 'fs'
-import {
-  apiCall
-} from "./../utils"
+import { apiCall } from "./../utils"
 
 let rawdata = fs.readFileSync(__dirname + '/../../../credentials/googleBooksConfig.json')
 let config = JSON.parse(rawdata)
 
-const username = config.userId
-const apiKey = config.bookshelfId
+const userId = config.userId
+const bookshelfId = config.bookshelfId
 
-const API_ENDPOINT = `https://www.googleapis.com/books/v1/users/${username}/bookshelves/${apiKey}/volumes?maxResults=40`
+const BATCH_SIZE = 40
+const API_VOLUMES_ENDPOINT = `https://www.googleapis.com/books/v1/users/${userId}/bookshelves/${bookshelfId}/volumes?maxResults=${BATCH_SIZE}`
+const API_BOOKSHELF_ENDPOINT = `https://www.googleapis.com/books/v1/users/${userId}/bookshelves/${bookshelfId}`
 
 // orchestrates the fetching of raw data, and then normalising it for external use
 async function fetchBooks() {
-  const rawBookData = await apiCall(API_ENDPOINT)
+  const bookshelfInformation = await fetchBookshelfInformation()
+  const rawBookData = await fetchRawBooksInfo(bookshelfInformation)
   const resultData = extractRelevantApiData(rawBookData)
 
   return resultData
+}
+
+async function fetchRawBooksInfo(bookshelfInformation) {
+  const numBooks = bookshelfInformation.volumeCount
+  const links = generateApiLinks(numBooks)
+
+  const apiResponses = await Promise.all(links.map((link) => {
+    return apiCall(link)
+  }))
+
+  return apiResponses.reduce(
+    (accumulator, response) => accumulator.concat(response.items),
+    []
+  )
+}
+
+function generateApiLinks(numBooks) {
+  const linksNeeded = ceil(numBooks/BATCH_SIZE)
+  return fill(Array(linksNeeded), '')
+    .map((_, index) => `${API_VOLUMES_ENDPOINT}&startIndex=${index*BATCH_SIZE}`)
+}
+
+async function fetchBookshelfInformation() {
+  return await apiCall(API_BOOKSHELF_ENDPOINT)
 }
 
 // normalises the data from the GoogleBooks API
