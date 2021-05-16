1394ce400955a5ff95cc552d504a9ff596a729ea
RyanMKrol <ryankrol.m@gmail.com>
2021-05-16 20:12:32 +0100

    Remove default logger

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 // app.js
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import logger from 'morgan';
 import createError from 'http-errors';
 import cors from 'cors';
 
@@ -10,7 +9,6 @@ import exampleRouter from './routes/example';
 const app = express();
 
 app.use(cors());
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -20,18 +18,6 @@ app.use('/example/endpoint', exampleRouter);
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
-});
-
-// error handler
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.send('500 - Internal Server Error');
 });
 
 export default app;
