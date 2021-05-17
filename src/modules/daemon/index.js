d0101dbca2a0881a0fd49ad1b5e1cc5e7f4a16d1
RyanMKrol <ryankrol.m@gmail.com>
2021-05-17 21:46:26 +0100

    Add sleep to prime daemon
    * This will give the caches some time to populate their
    data from long term storage before attempting to hit them.

--- src/modules/daemon/index.js
+++ src/modules/daemon/index.js
@@ -1,4 +1,5 @@
 import schedule from 'node-schedule';
+import { sleep } from 'noodle-utils';
 import primeSite from './prime';
 
 /**
@@ -12,6 +13,8 @@ async function runDaemons() {
  * Function to start running/scheduling all daemons used by the site
  */
 async function main() {
+  await sleep(1000 * 60);
+
   runDaemons();
 
   schedule.scheduleJob('0 0 0 * * *', async () => {
